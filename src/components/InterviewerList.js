7bc6e9599fdd00961b095a5e59fdf1658464cafe
isaiahiu <isaiah.iu@hotmail.com>
2022-04-19 17:05:55 -0400

    Create components and add to stories with styles
    - Create Button and its scss file, tested in stories.
    - Create DayListItem and its scss file, tested in stories.
    - Create DayList, tested in stories.
    - Create InterviewerListItem and its scss file, tested in stories.
    - Create InterviewerList, tested in stories.

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props) {
+	const list = props.interviewers.map(person => {
+		return (
+			<InterviewerListItem
+				id={person.id}
+				name={person.name}
+				avatar={person.avatar}
+				selected={props.interviewer === person.id}
+				setInterviewer={props.setInterviewer}
+				interviewer={props.interviewer}
+			/>
+		);
+	});
+	return (
+		<section className='interviewers'>
+			<h4 className='interviewers__header text--light'>Interviewer</h4>
+			<ul className='interviewers__list'>{list}</ul>
+		</section>
+	);
+}
