4a079e034aaa55e91482afa3c05c90c1b9ae09ba
isaiahiu <isaiah.iu@hotmail.com>
2022-04-19 19:15:47 -0400

    Create Appointment folder within components folder
    - Create Confirm JSX and add to stories.
    - Create Empty JSX and add to stories.
    - Create Error JSX and add to stories.
    - Create Header JSX and add to stories.
    - Create index JSX and add to stories.
    - Create Show JSX and add to stories.
    - Create Status JSX and add to stories.
    - Create styles.scss file.

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,13 @@ import "index.scss";
 import Button from "components/Button";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm.js";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
 	.addParameters({
@@ -124,5 +131,36 @@ storiesOf("InterviewerList", module)
 		<InterviewerList
 			interviewers={interviewers}
 			onChange={action("setInterviewer")}
+		/>
+	));
+
+storiesOf("Appointment", module)
+	.addParameters({
+		backgrounds: [{ name: "white", value: "#fff", default: true }],
+	})
+	.add("Appointment", () => <Appointment />)
+	.add("Appointment with Time", () => <Appointment time='12pm' />)
+	.add("Header", () => <Header time='12pm' />)
+	.add("Empty", () => <Empty onAdd={action("onAdd")} />)
+	.add("Show", () => (
+		<Show
+			student='Lydia Miller-Jones'
+			interviewer={interviewer}
+			onEdit={action("onEdit")}
+			onDelete={action("onDelete")}
+		/>
+	))
+	.add("Confirm", () => (
+		<Confirm
+			message='Delete the appointment?'
+			onConfirm={action("onConfirm")}
+			onCancel={action("onCancel")}
+		/>
+	))
+	.add("Status", () => <Status message='Deleting' />)
+	.add("Error", () => (
+		<Error
+			message='Could not delete appointment.'
+			onClose={action("onClose")}
 		/>
 	));
