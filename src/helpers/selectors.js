77d1ccedeab869c1b79aa99f4b8f1759b7e8d334
isaiahiu <isaiah.iu@hotmail.com>
2022-04-20 15:50:07 -0400

    Create selectors.js and getAppointmentsForDay fn

---
+++ src/helpers/selectors.js
@@ -0,0 +1,16 @@
+export function getAppointmentsForDay(state, day) {
+	//find object in state.days array where name = name
+	const daySearch = state.days.filter(current => {
+		return current.name === day;
+	});
+	// once have days array, iterate and compare id with states.appointment
+	if (daySearch.length > 0) {
+		const appts = daySearch[0].appointments.map(apptID => {
+			return state.appointments[apptID];
+		});
+		// if match return value
+		return appts;
+	}
+	// if no appointments, return empty array
+	return daySearch;
+}
